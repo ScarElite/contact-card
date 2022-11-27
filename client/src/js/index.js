a5751b0e1a28d2352eec82ab22e7a338da569593
Mitchell Armstrong <mra0211@gmail.com>
2022-11-27 16:18:58 -0500

    Setup the indexedDB

--- client/src/js/index.js
+++ client/src/js/index.js
@@ -3,6 +3,7 @@ import "./form";
 import "./submit";
 import { Tooltip, Toast, Popover } from "bootstrap";
 import "bootstrap/dist/css/bootstrap.min.css";
+import { initdb } from "./database";
 
 // Import images
 import Logo from "../images/logo.png";
@@ -11,6 +12,7 @@ import Dog from "../images/dog.png";
 
 // Add images on load
 window.addEventListener("load", function () {
+  initdb();
   document.getElementById("logo").src = Logo;
   document.getElementById("bearThumbnail").src = Bear;
   document.getElementById("dogThumbnail").src = Dog;
