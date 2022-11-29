f647e1cd32dfd864eca42ab40a34bd6ffad48ff4
Mitchell Armstrong <mra0211@gmail.com>
2022-11-29 12:45:38 -0500

    Created the functionality to delete a card from the UI and the indexeddb database

--- client/src/js/index.js
+++ client/src/js/index.js
@@ -2,7 +2,7 @@
 import "./form";
 import { Tooltip, Toast, Popover } from "bootstrap";
 import "bootstrap/dist/css/bootstrap.min.css";
-import { initdb, getDb, postDb } from "./database";
+import { initdb, getDb, postDb, deleteDb } from "./database";
 import { fetchCards } from "./cards";
 import { toggleForm } from "./form";
 import { clearForm } from "./form";
@@ -55,6 +55,15 @@ form.addEventListener("submit", (event) => {
   // Reload the DOM
   fetchCards();
 });
+
+window.deleteCard = (e) => {
+  // Grabs the id from the button element attached to the contact card
+  let id = parseInt(e.id);
+  // Delete the card
+  deleteDb(id);
+  // Reload the DOM
+  fetchCards();
+};
 
 // Import CSS files
 import "../css/index.css";
