4edf11b621648c80dd656b2f71a08d438a19bbde
Mitchell Armstrong <mra0211@gmail.com>
2022-11-29 13:03:03 -0500

    Created the functionality to update the cards

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -78,11 +78,35 @@ export const deleteDb = async (id) => {
   // Open up the desired object store
   const store = tx.objectStore("contacts");
 
-  // Use the .delete() method to get all data in the database
+  // Use the .delete() method to delete data in the database
   const request = store.delete(id);
 
   // Get confirmation of the request
   const result = await request;
   console.log("result.value", result);
   return result?.value;
+};
+
+export const editDb = async (id, name, email, phone, profile) => {
+  console.log("UPDATE from the database", id);
+
+  // Create a connection to the IndexedDB database and the version we want to use
+  const contactDb = await openDB("contact_db", 1);
+
+  // Create a new transaction and specify the store and the data privileges
+  const tx = contactDb.transaction("contacts", "readwrite");
+
+  // Open up the desired object store
+  const store = tx.objectStore("contacts");
+
+  // Use the .put() method to update data in the database
+  const request = store.put({
+    id: id,
+    name: name,
+    email: email,
+    phone: phone,
+    profile: profile,
+  });
+  const result = await request;
+  console.log("🚀 - data saved to the database", result);
 };
