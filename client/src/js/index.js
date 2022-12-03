46ea4dd42df4a64e0404ae10ad351b9f73118f74
Mitchell Armstrong <mra0211@gmail.com>
2022-12-03 12:03:30 -0500

    Created installation functionality

--- client/src/js/index.js
+++ client/src/js/index.js
@@ -21,6 +21,24 @@ window.addEventListener("load", function () {
   document.getElementById("dogThumbnail").src = Dog;
 });
 
+// Install Button
+const installBtn = document.getElementById("installBtn");
+
+window.addEventListener("beforeinstallprompt", (event) => {
+  event.preventDefault();
+  installBtn.style.visibility = "visible";
+
+  installBtn.addEventListener("click", () => {
+    event.prompt();
+    installBtn.setAttribute("disabled", true);
+    installBtn.textContent = "Installed!";
+  });
+
+  window.addEventListener("appinstalled", (event) => {
+    console.log("👍", "appinstalled", event);
+  });
+});
+
 // Form functionality
 const form = document.getElementById("formToggle");
 const newContactButton = document.getElementById("new-contact");
